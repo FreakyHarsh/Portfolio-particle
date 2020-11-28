4351ce8892b3dfd7eab5da9e8319073cc6c30f4b
FreakyHarsh <harsh.boricha2015@gmail.com>
2020-11-28 16:47:41 +0530

    particle background added

---
+++ src/components/ParticleContainer/ParticleContainer.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+function ParticleContainer(props) {
+  return (
+    <>
+      <Particles
+        height="100vh"
+        width="100%"
+        params={{
+          particles: {
+            number: {
+              value: 80,
+              density: {
+                enable: true,
+                value_area: 2000,
+              },
+            },
+            color: {
+              value: '#ffffff',
+            },
+            opacity: {
+              value: 0.5,
+              random: false,
+              anim: {
+                enable: false,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false,
+              },
+            },
+            size: {
+              value: 3,
+              random: true,
+              anim: {
+                enable: false,
+                speed: 19.18081918081918,
+                size_min: 0.1,
+                sync: false,
+              },
+            },
+            line_linked: {
+              enable: true,
+              distance: 150,
+              color: '#ffffff',
+              opacity: 0.4,
+              width: 1,
+            },
+            move: {
+              enable: true,
+              speed: 4,
+              direction: 'none',
+              random: true,
+              straight: false,
+              out_mode: 'out',
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200,
+              },
+            },
+            nb: 80,
+          },
+          interactivity: {
+            detect_on: 'canvas',
+            events: {
+              onhover: {
+                enable: true,
+                mode: 'grab',
+              },
+              onclick: {
+                enable: true,
+                mode: 'push',
+              },
+              resize: true,
+            },
+            modes: {
+              grab: {
+                distance: 400,
+                line_linked: {
+                  opacity: 1,
+                },
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+              push: {
+                particles_nb: 4,
+              },
+              remove: {
+                particles_nb: 2,
+              },
+            },
+          },
+          retina_detect: true,
+        }}
+        style={{
+          backgroundColor: '#1a222c',
+        }}
+      />
+    </>
+  );
+}
+
+export default ParticleContainer;
