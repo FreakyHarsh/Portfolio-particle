efda18ee3372199b3fd5c4f8f4e83f5268b0104b
FreakyHarsh <harsh.boricha2015@gmail.com>
2020-12-03 12:38:34 +0530

    was testing cli integration: failed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+
 import HeaderContent from './components/HeaderContent/HeaderContent';
 import Projects from './components/Projects/Projects';
 import Aboutme from './components/Aboutme/Aboutme';
 import FooterContent from './components/FooterContent/FooterContent';
+
 function App() {
   return (
     <div>
